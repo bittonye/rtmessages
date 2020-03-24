1dea6faf1ec6fa839ae8806c27468a6e13727b70
Lolporer <lolporer@gmail.com>
2020-03-24 08:31:07 +0200

    support timeout for updates and change of id

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 
 const UPDATE_INTERVAL = 100; /* 100 ms interval for client updates */
+const UPDATE_TIMEOUT = 5 * 60 * 1000; /* 5 minutes interval */
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 
@@ -14,24 +15,36 @@ const server = http.createServer(app);
 const io = socketIo(server); // < Interesting!
 
 const users = {};
-        
+
+/* Set connection interval for broadcast */
+setInterval(
+    () => {
+        io.emit("update locations", users); /* Broadcast message to all clients */
+    },
+    UPDATE_INTERVAL,
+);
+
 io.on("connection", socket => {
-    const uid = socket.request.headers['x-forwarded-for'] || socket.request.connection.remoteAddress;
+    let uid = socket.id;
     console.log('New request from : ' + uid);
-    users[uid] = {};
-    setInterval(
-        () => {
-            io.emit("update locations", users); /* Broadcast message to all clients */
-        },
-        UPDATE_INTERVAL,
-    );
+
+    let updateTimeout = setTimeout(() => {
+        delete users[uid];
+    }, UPDATE_TIMEOUT);
+
+    socket.on("update id", function(id) {
+        users[id] = users[uid];
+        delete users[uid];
+        let uid = id;
+    });
     socket.on("update location", function(msg){
+        updateTimeout.refresh();
         users[uid] = msg;
         console.table(msg);
     });
     socket.on("disconnect", () => {
         console.log("Client disconnected");
-        users[uid] = {};
+        delete users[uid];
     });
 });
 
